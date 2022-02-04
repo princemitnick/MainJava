5e2713550f937931a894ffdd6d1725b3639d9831
Jean Baptiste Prince Stanley Lovensky J <ingjeanbaptiste@gmail.com>
2022-02-04 12:38:51 -0500

    Initial Commit : MainJava project

---
+++ src/com/prince/java/Switch_cases.java
@@ -0,0 +1,34 @@
+package com.prince.java;
+
+import java.util.Scanner;
+
+public class Switch_cases {
+
+    public static void main(String... args){
+        double value1 = 500d;
+        double value2 = 100d;
+        double result = 0.0d;
+        char opCode = 'd';
+
+
+        switch (opCode){
+            case 'a':
+                result = value1 + value2;
+                break;
+            case 's':
+                result = value1 - value2;
+                break;
+            case 'm':
+                result = value1 * value2;
+                break;
+            case 'd':
+                result = value1 / value2;
+                break;
+            default:
+                System.out.println("Invalid opCode: "+opCode);
+                result = 0.0d;
+        }
+        System.out.println(result);
+
+    }
+}
