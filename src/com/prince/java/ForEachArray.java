4361dcd819468d890ffb791750f303acb93b0902
Jean Baptiste Prince Stanley Lovensky J <ingjeanbaptiste@gmail.com>
2022-02-05 12:53:54 -0500

    ForEachArray file added

--- src/com/prince/java/ForEachArray.java
+++ src/com/prince/java/ForEachArray.java
@@ -1,4 +1,23 @@
 package com.prince.java;
 
 public class ForEachArray {
+
+
+    public static void main(String... args){
+        double [] leftVals = {100.4d, 804.5d, 203.6d, 45.6d, 130.2d};
+        String [] names = {"Jean", "Baptiste", "Prince", "Stanley", "Lovensky", "J"};
+
+        for (double result: leftVals
+             ) {
+            System.out.println(result);
+        }
+
+        for (
+                String name: names
+        ){
+            System.out.println(name);
+        }
+    }
+
+
 }
