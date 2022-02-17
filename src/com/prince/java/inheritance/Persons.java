65548d6a30fadbc8976c3bb9658831d8ace8b20b
Jean Baptiste Prince Stanley Lovensky J <ingjeanbaptiste@gmail.com>
2022-02-17 13:06:39 -0500

    Override equals method

--- src/com/prince/java/inheritance/Persons.java
+++ src/com/prince/java/inheritance/Persons.java
@@ -1,5 +1,7 @@
 package com.prince.java.inheritance;
 
+import java.util.Objects;
+
 public class Persons {
 
     private String code;
@@ -13,12 +15,31 @@ public class Persons {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (! (obj instanceof Persons))
-            return false;
-        Persons persons = (Persons) obj;
-        return code == persons.code && nom == persons.nom &&
-                prenom == persons.prenom;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Persons persons = (Persons) o;
+        return code.equals(persons.code) && nom.equals(persons.nom) && prenom.equals(persons.prenom);
     }
 
+    /*  @Override
+        public boolean equals(Object obj) {
+            if (! (obj instanceof Persons))
+                return false;
+            Persons persons = (Persons) obj;
+            return code == persons.code && nom == persons.nom &&
+                    prenom == persons.prenom;
+        }
+    */
+    public static void main(String... args){
+        Persons p = new Persons("001","P","S");
+        Persons p2 = new Persons("001","P","S");
+
+        System.out.println(p.equals(p2));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, nom, prenom);
+    }
 }
