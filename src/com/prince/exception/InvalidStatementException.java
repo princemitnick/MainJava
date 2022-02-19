60ed93842289edfcd9b9a077257f9bc7e638c9d8
Jean Baptiste Prince Stanley Lovensky J <ingjeanbaptiste@gmail.com>
2022-02-19 12:35:58 -0500

    Create exception class

---
+++ src/com/prince/exception/InvalidStatementException.java
@@ -0,0 +1,10 @@
+package com.prince.exception;
+
+public class InvalidStatementException extends Exception {
+
+    public InvalidStatementException (String message){
+        super(message);
+    }
+
+
+}
